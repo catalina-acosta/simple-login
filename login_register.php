98a4a6350df97d8725aed63ab0092e3cffdcdb07
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-06-03 11:59:30 +0200

    added condition to handle login after form submission and checks if the data of the form matches the data in the db.

--- login_register.php
+++ login_register.php
@@ -3,7 +3,8 @@
 session_start();
 require_once 'config.php';
 
-if (isset($_POST['register'])) {
+// Condition to handle registration
+if (isset($_POST['register'])) { // listens to the action of the form register
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -20,4 +21,31 @@ if (isset($_POST['register'])) {
     header("Location: index.php");
     exit();
 }
+
+// condition to handle login
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role'] === 'admin') {
+                header("Location: admin_page.php");
+            } else {
+                header("Location: user_page.php");
+            }
+            exit();
+        }
+    }
+}
+
+$_SESSION['login_error'] = 'Incorrect email or password';
+$_SESSION['active_form'] = 'login';
+header("Location: index.php");
+
 ?>
