e21902f8f82e77465dca3a3b76aada8c16b36788
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-06-03 14:24:59 +0200

    added basic styling to admin and user views

--- user_page.php
+++ user_page.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['email'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./styles.css">
 </head>
 <body style="background: #fff:">
     <div class="box">
